91dd1baa382b65f398effd262b14c096a15ad3d8
TobisMa <tobias-ma@gmx.net>
2022-12-07 13:43:55 +0100

    Added act cycle

--- pygreenfoot/__init__.py
+++ pygreenfoot/__init__.py
@@ -2,3 +2,10 @@ import pygame
 from .application import Application
 from .gameObject import GameObject
 from .scene import Scene
+from . import keys
+from .event import Event, EventType
+
+# assigning the values to the key elements in keys
+__keys = {k for k in dir(pygame.constants) if k.startswith("K_")}
+for __k in __keys:
+    setattr(keys, __k, getattr(pygame.constants, __k))
--- pygreenfoot/scene.py
+++ pygreenfoot/scene.py
@@ -1,5 +1,5 @@
-from abc import ABCMeta
-from typing import TYPE_CHECKING, DefaultDict, Iterable, Iterator, Set
+from abc import ABCMeta, abstractmethod
+from typing import TYPE_CHECKING, DefaultDict, Iterable, Iterator, List, Set, Type
 from vectormath import Vector2Array
 
 from pygreenfoot.gameObject import GameObject
@@ -7,18 +7,22 @@ from pygreenfoot.gameObject import GameObject
 
 class Scene(metaclass=ABCMeta):
     
-    __slots__ = ("__size", "world_bounding", "__field_size", "__objects")
+    __slots__ = ("__size", "world_bounding", "__field_size", "__objects", "__existing_object_types")
+    
+    __render_order: List[Type[GameObject]] = []
     
     def __init__(self, width: int, height: int, field_size: int, world_bounding: bool = True) -> None:
         self.__size = Vector2Array(width, height)
         self.__field_size = field_size
         self.world_bounding = world_bounding
-        self.__objects = DefaultDict(set)
+        self.__objects: DefaultDict[Type[GameObject], Set[GameObject]] = DefaultDict(set)
+        self.__existing_object_types = set()
         
     def add_to_scene(self, game_object: "GameObject") -> None:
         if not isinstance(game_object, GameObject):
             raise TypeError("Only subclasses of GameObject")
         self.__objects[type(game_object)].add(game_object)
+        self.__existing_object_types.add(type(game_object))
         game_object.on_scene_add(self)
         
     @property
@@ -32,3 +36,30 @@ class Scene(metaclass=ABCMeta):
     @property
     def field_size(self) -> int:
         return self.__field_size
+    
+    def _calc_frame(self) -> None:
+        self.act()
+        done: Set[Type[GameObject]] = set()
+        for object_type in Scene.__render_order:
+            done.add(object_type)
+            for game_object in self.__objects[object_type]:
+                game_object.act()
+        
+        for object_type in set(self.__objects) - done:
+            done.add(object_type)
+            for game_object in self.__objects[object_type]:
+                game_object.act()
+            
+    
+    @abstractmethod
+    def act(self) -> None:
+        raise NotImplementedError("act method needs to be implemented")
+    
+    
+    @classmethod
+    def set_render_order(cls, render_order: Iterable) -> None:
+        cls.__render_order = list(render_order)
+        
+    @classmethod
+    def get_render_order(cls) -> List[Type[GameObject]]:
+        return cls.__render_order
