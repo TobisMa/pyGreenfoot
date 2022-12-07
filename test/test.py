97b86b2e35792f070345b0a4ac9cf46fa1226089
TobisMa <tobias-ma@gmx.net>
2022-12-07 13:59:12 +0100

    new test file

--- test/test.py
+++ test/test.py
@@ -1,18 +1,17 @@
 import sys
+
 sys.path.append(".")
-
-from pygreenfoot.scene import Scene
-
-
 from pygreenfoot import Application
+from pygreenfoot import EventType
+from pygreenfoot import Scene
+from pygreenfoot import keys
 from test_scene import TestScene
+
 app = Application()
 app.add_scene(TestScene)
 app.current_scene = TestScene()
 app.start()
 
 while (app.is_running()):
-    for event in app.get_events():
-        pass
     app.update()
     
