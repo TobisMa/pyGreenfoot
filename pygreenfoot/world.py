52b68096f6a903680d4bcb9cda4cac160e0e50b8
TobisMa <tobias-ma@gmx.net>
2022-12-14 22:13:22 +0100

    images for actors

--- pygreenfoot/world.py
+++ pygreenfoot/world.py
@@ -1,3 +1,4 @@
+import pygame
 from abc import ABCMeta, abstractmethod
 from typing import TYPE_CHECKING, DefaultDict, Iterable, Iterator, List, Set, Type
 from vectormath import Vector2Array
@@ -8,14 +9,15 @@ from pygreenfoot.actor import Actor
 class World(metaclass=ABCMeta):
     
     __slots__ = ("__size", "world_bounding", "__cell_size", "__objects", 
-                 "__existing_object_types", "__render_order")
+                 "__existing_object_types", "__act_order", "__paint_order")
     
     
     def __init__(self, width: int, height: int, field_size: int, world_bounding: bool = True) -> None:
         self.__size = Vector2Array(width, height)
-        self.__cell_size = field_size
-        self.world_bounding = world_bounding
-        self.__render_order: List[Type[Actor]] = []
+        self.__cell_size: int = field_size
+        self.world_bounding: bool = world_bounding
+        self.__act_order: List[Type[Actor]] = []
+        self.__paint_order: List[Type[Actor]] = []
         self.__objects: DefaultDict[Type[Actor], Set[Actor]] = DefaultDict(set)
         self.__existing_object_types = set()
         
@@ -61,10 +63,27 @@ class World(metaclass=ABCMeta):
         """
         return self.__cell_size
     
-    def _calc_frame(self) -> None:
+    def _calc_frame(self, screen: pygame.Surface) -> None:
+        self.__act_cycle()
+        self.__draw_cycle(screen)
+        
+    def __draw_cycle(self, screen: pygame.Surface) -> None:
+        done: Set[Type[Actor]] = set()
+        for object_type in self.__paint_order:
+            done.add(object_type)
+            for game_object in self.__objects[object_type]:
+                game_object.repaint(screen)
+        
+        for object_type in set(self.__objects) - done:
+            done.add(object_type)
+            for game_object in self.__objects[object_type]:
+                game_object.repaint(screen)
+                
+                
+    def __act_cycle(self) -> None:
         self.act()
         done: Set[Type[Actor]] = set()
-        for object_type in self.__render_order:
+        for object_type in self.__act_order:
             done.add(object_type)
             for game_object in self.__objects[object_type]:
                 game_object.act()
@@ -80,14 +99,20 @@ class World(metaclass=ABCMeta):
         """Called once per frame by the main application"""
         raise NotImplementedError("act method needs to be implemented")
     
-    def set_render_order(self, render_order: Iterable[Type[Actor]]) -> None:
-        """Sets the order in which actors are being *calculated*
+    def set_act_order(self, act_order: Iterable[Type[Actor]]) -> None:
+        """Sets the order in which the actor's act methods are called
 
         Args:
-            render_order (Iterable): _description_
+            act_order (Iterable): _description_
         """
-        self.__render_order = list(render_order)
+        self.__act_order = list(act_order)
         
-    def get_render_order(self) -> List[Type[Actor]]:
-        return self.__render_order
+    def get_act_order(self) -> List[Type[Actor]]:
+        return self.__act_order
+    
+    def set_paint_order(self, paint_order: Iterable[Type[Actor]]) -> None:
+        self.__paint_order = list(paint_order)
+        
+    def get_paint_order(self) -> List[Type[Actor]]:
+        return self.__paint_order
     
