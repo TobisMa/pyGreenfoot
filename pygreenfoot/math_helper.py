57bd22837d8525374f16cf60e3d2d4fb53ed1826
TobisMa <tobias-ma@gmx.net>
2022-12-15 22:42:17 +0100

    background image & actor bounding

--- pygreenfoot/math_helper.py
+++ pygreenfoot/math_helper.py
@@ -1,4 +1,15 @@
+from typing import TypeVar
 from math import pi as PI
+
+__Number = TypeVar("__Number", int, float)
 
 
 FULL_DEGREES_ANGLE = 360.0
+
+
+def limit_value(value: __Number, min_: __Number, max_: __Number) -> __Number:
+    if value < min_:
+        return min_
+    elif value > max_:
+        return max_
+    return value
--- pygreenfoot/world.py
+++ pygreenfoot/world.py
@@ -1,6 +1,8 @@
+import itertools
+import os
 import pygame
 from abc import ABCMeta, abstractmethod
-from typing import TYPE_CHECKING, DefaultDict, Iterable, Iterator, List, Set, Type
+from typing import TYPE_CHECKING, DefaultDict, Iterable, Iterator, List, Optional, Set, Type, Union
 from vectormath import Vector2Array
 
 from pygreenfoot.actor import Actor
@@ -9,17 +11,19 @@ from pygreenfoot.actor import Actor
 class World(metaclass=ABCMeta):
     
     __slots__ = ("__size", "world_bounding", "__cell_size", "__objects", 
-                 "__existing_object_types", "__act_order", "__paint_order")
+                 "__existing_object_types", "__act_order", "__paint_order",
+                 "__background")
     
     
-    def __init__(self, width: int, height: int, field_size: int, world_bounding: bool = True) -> None:
+    def __init__(self, width: int, height: int, cell_size: int, world_bounding: bool = True) -> None:
         self.__size = Vector2Array(width, height)
-        self.__cell_size: int = field_size
+        self.__cell_size: int = cell_size
         self.world_bounding: bool = world_bounding
         self.__act_order: List[Type[Actor]] = []
         self.__paint_order: List[Type[Actor]] = []
         self.__objects: DefaultDict[Type[Actor], Set[Actor]] = DefaultDict(set)
         self.__existing_object_types = set()
+        self.__background: Optional[pygame.Surface] = None
         
     def add_to_world(self, game_object: "Actor") -> None:
         """Adds an actor to this world instance
@@ -63,21 +67,24 @@ class World(metaclass=ABCMeta):
         """
         return self.__cell_size
     
-    def _calc_frame(self, screen: pygame.Surface) -> None:
+    def _calc_frame(self, screen: pygame.Surface) -> None:                 
         self.__act_cycle()
         self.__draw_cycle(screen)
         
     def __draw_cycle(self, screen: pygame.Surface) -> None:
+        if self.__background is not None:
+            screen.blit(self.__background, (0, 0))
+            
         done: Set[Type[Actor]] = set()
         for object_type in self.__paint_order:
             done.add(object_type)
             for game_object in self.__objects[object_type]:
-                game_object.repaint(screen)
+                game_object.repaint(screen, self)
         
         for object_type in set(self.__objects) - done:
             done.add(object_type)
             for game_object in self.__objects[object_type]:
-                game_object.repaint(screen)
+                game_object.repaint(screen, self)
                 
                 
     def __act_cycle(self) -> None:
@@ -115,4 +122,26 @@ class World(metaclass=ABCMeta):
         
     def get_paint_order(self) -> List[Type[Actor]]:
         return self.__paint_order
+    
+    def set_background(self, filename_or_image: Union[str, pygame.Surface]) -> None:
+        if isinstance(filename_or_image, str):
+            path = os.path.join("images", filename_or_image)
+            if not os.access(path, os.F_OK):
+                raise FileNotFoundError("File \"" + path + "\" not found")
+
+            image: pygame.Surface = pygame.image.load(path)
+            
+        else:
+            image: pygame.Surface = filename_or_image
+        
+        bg = pygame.Surface((self.width * self.cell_size, self.height * self.cell_size))
+        wr = range(0, self.cell_size * self.width + self.cell_size, self.cell_size)
+        hr = range(0, self.cell_size * self.height + self.cell_size, self.cell_size)
+        
+        for x in wr:
+            for y in hr:
+                bg.blit(image.copy(), (x, y))
+        
+        self.__background = bg
+            
     
