2662b16069b13148133d7119d5f624177dde47a8
TobisMa <tobias-ma@gmx.net>
2022-12-15 18:56:19 +0100

    backspace actually works

--- pygreenfoot/pygreenfoot.py
+++ pygreenfoot/pygreenfoot.py
@@ -49,7 +49,7 @@ class PyGreenfoot:
                     return app.get_key_states(keys.K_LALT)[0]
                 elif key == "alt gr":
                     return app.get_key_states(keys.K_RALT)[0]
-                elif key == "backspace": # FIXME not recognized
+                elif key == "backspace":
                     return app.get_key_states(keys.K_BACKSPACE)[0]
                 elif key == "delete":
                     return app.get_key_states(keys.K_DELETE)[0]
