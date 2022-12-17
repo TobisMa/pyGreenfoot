476c95f6b59caa56cf63e4bf77f12b7e0b66a254
TobisMa <tobias-ma@gmx.net>
2022-12-17 10:53:49 +0100

    color constants

--- pygreenfoot/color.py
+++ pygreenfoot/color.py
@@ -10,3 +10,14 @@ class Color:
     @property
     def _pygame(self) -> Tuple[int, int, int]:
         return self.r, self.g, self.b
+
+
+BLACK = Color(0, 0, 0)
+RED = Color(255, 0, 0)
+GREEN = Color(0, 255, 0)
+BLUE = Color(0, 0, 255)
+YELLOW = Color(255, 255, 0)
+VIOLET = Color(255, 0, 255)
+CYAN = Color(0, 255, 255)
+WHITE = Color(255, 255, 255)
+GRAY = Color(128, 128, 128)
