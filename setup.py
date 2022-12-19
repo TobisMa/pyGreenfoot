e214afed2652215787bcf14025797701f2bb05f6
TobisMa <tobias-ma@gmx.net>
2022-12-19 11:19:37 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='PyGreenfoot',
+    version="1.0.0",
+    description='The java greenfoot in a python package',
+    packages=['pygreenfoot', 'pygreenfoot.templates'],
+    license="MIT License"
+)
