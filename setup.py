a176ee04e3da5b0ac81dffa931429f615ad3dd13
TobisMa <tobias-ma@gmx.net>
2022-12-19 11:24:28 +0100

    better setup file

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ setup(
     version="1.0.0",
     description='The java greenfoot in a python package',
     packages=['pygreenfoot', 'pygreenfoot.templates'],
-    license="MIT License"
+    license="MIT License",
+    python_requires=">=3.6.1, <4",
+    install_requires=[
+        "pygame >= 2.1.2",
+        "typing"
+    ],
 )
