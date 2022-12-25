9c801d7779a36758900af3206ca828c599ab61fb
TobisMa <tobias-ma@gmx.net>
2022-12-25 02:31:38 +0100

    math helper comments

--- pygreenfoot/math_helper.py
+++ pygreenfoot/math_helper.py
@@ -7,6 +7,16 @@ FULL_DEGREES_ANGLE = 360.0
 
 
 def limit_value(value: __Number, min_: __Number, max_: __Number) -> __Number:
+    """Limit a value to a minimum and maximum
+
+    Args:
+        value (__Number): the value
+        min_ (__Number): the allowed minimum
+        max_ (__Number): the allowed maximum
+
+    Returns:
+        __Number: the resulting value if the boundaries have been checked.
+    """
     if value < min_:
         return min_
     elif value > max_:
