a0c86dd2a604ff9fff3ec5db309b9fa7dd61f5f5
TobisMa <tobias-ma@gmx.net>
2023-01-15 13:33:36 +0100

    timer template

---
+++ pygreenfoot/templates/timer.py
@@ -0,0 +1,32 @@
+from math import floor
+import time
+from typing import Dict, Hashable
+from pygreenfoot import Actor
+
+
+class Timer:
+    
+    __slots__ = ("__tags", )
+    
+    def __init__(self) -> None:
+        self.__tags: Dict[Hashable, int] = {}
+        
+    def mark(self, tag: Hashable = None) -> None:
+        """Sets a timer (in nanoseconds) on tag. A tag does not need to be provided.
+
+        Args:
+            tag (Hashable, optional): the tag to set the timer on. If already set, resets the timer. Defaults to None.
+        """
+        self.__tags[tag] = time.perf_counter_ns()
+        
+    def elapsed_ns(self, tag: Hashable = None) -> int:
+        """Return the elapsed time on the given tag (in nanoseconds)
+
+        Args:
+            tag (Hashable, optional): the tag to look for. Defaults to None.
+
+        Returns:
+            int: the elapsed time in nanoseconds
+        """
+        current_time = time.perf_counter_ns()
+        return current_time - self.__tags[tag]
