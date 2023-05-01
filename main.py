1c6e7596d78d56c936699aacb991d57c64cda8e9
TobisMa <tobias-ma@gmx.net>
2023-05-01 12:48:33 +0200

    small changes

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from pygreenfoot import Application
 
-if __name__ == '__main__':	Application.main(())
+if __name__ == '__main__':	Application.main()
--- pygreenfoot/templates/init_actor.py
+++ pygreenfoot/templates/init_actor.py
@@ -1,11 +1,11 @@
 from abc import abstractmethod
 from typing import Optional
-from pygreenfoot import Actor
-from pygreenfoot import Image
+
+from pygreenfoot import Actor, Image
 
 
 class InitActor(Actor):
-    
+
     __slots__ = ("__init",)
     
     def __init__(self, rotation: int = 0, image: Optional[Image] = None) -> None:
