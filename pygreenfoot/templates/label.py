19f33b497cf1ed4ac5771642a125ae83916f5e76
TobisMa <tobias-ma@gmx.net>
2023-05-03 21:38:13 +0200

    label template

---
+++ pygreenfoot/templates/label.py
@@ -0,0 +1,50 @@
+from typing import Any, Optional
+from pygreenfoot.actor import Actor
+from pygreenfoot.color import Color
+from pygreenfoot.font import Font, Text
+from pygreenfoot.image import Image
+
+
+class Label(Actor):
+    
+    __slots__ = ("__text", "__font", "__background_color", "__updated")
+    
+    def __init__(self, text: Any, font: Font, rotation: int = 0, image: Optional[Image] = None):
+        Actor.__init__(self, rotation, image)
+        self.__text: str = str(text)
+        self.__font: Font = font
+        self.__background_color: Optional[Color] = None
+        self.__updated = True
+    
+    @property
+    def text(self) -> str:
+        return self.__text
+    
+    @text.setter
+    def text(self, value: Any) -> None:
+        self.__updated = True
+        self.__text = str(value)
+        
+    @property
+    def font(self) -> Font:
+        self.__updated
+        return self.__font
+    
+    @property
+    def background_color(self) -> Optional[Color]:
+        return self.__background_color
+    
+    @background_color.setter
+    def background_color(self, value: Optional[Color]) -> None:
+        self.__background_color = value
+        self.__updated = True
+    
+    def act(self):
+        if self.__updated:
+            image = self.get_image()
+            image.clear()
+            if self.__background_color:
+                image.fill_with_color(self.__background_color)
+            image.draw_text(self.text, 0, 0, self.__font)
+            self.set_image(image)
+    
