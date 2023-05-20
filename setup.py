ebc188782a913b28dd530ae4e27cb19400e99fea
TobisMa <tobias-ma@gmx.net>
2023-05-20 20:33:59 +0200

    include images in package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='PyGreenfoot',
     version="1.0.0",
     description='The java greenfoot in a python package',
-    packages=['pygreenfoot', 'pygreenfoot.templates'],
+    packages=['pygreenfoot', 'pygreenfoot.templates', 'pygreenfoot.images'],
     license="GNU General Public License, version 2, with the Classpath Exception",
     python_requires=">=3.6.1, <4",
     install_requires=[
@@ -12,6 +12,11 @@ setup(
         "typing"
     ],
     platforms=["linux", "mac", "win32"],
+    include_package_data=True,
+    package_dir={".": "pygreenfoot"},
+    package_data={
+        "": ["*.png", "*.jpg", "*.ico"]
+    },
     extras_requires={
         "plantuml image generation": ["plantuml>=0.3.0"]
     }
