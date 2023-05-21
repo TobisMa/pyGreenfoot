ce146a0c4a8950d9f5db4e1e61388dd41af8dadb
TobisMa <tobias-ma@gmx.net>
2023-05-21 22:35:54 +0200

    correct version in module

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from setuptools import find_packages, setup
+from string import whitespace
+from setuptools import setup
+
+def get_version():
+    with open("pygreenfoot/__init__.py", "r") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return line.split("=")[-1].strip(whitespace + "\"\'")
+    raise ValueError("No version in __init__.py")
 
 setup(
     name='PyGreenfoot',
-    version="1.0.0",
+    version=get_version(),
+    setup_requires=["setuptools_scm", "setuptools", ],
     description='The java greenfoot in a python package',
     packages=['pygreenfoot', 'pygreenfoot.templates', 'pygreenfoot.images'],
     license="GNU General Public License, version 2, with the Classpath Exception",
